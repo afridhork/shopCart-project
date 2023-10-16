a7d51452747588cddbd89a0f7a53bf235082c45d
Afridho R Kartawiria <afridhorkartawiria@gmail.com>
2023-10-16 15:52:34 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
    reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+   async redirects() {
+     return [
+       {
+         source: '/category/[slug]/page.tsx',
+         destination: '/category/[...slug]/page.tsx',
+         permanent: false,
+       },
+     ];
+   },
+}
